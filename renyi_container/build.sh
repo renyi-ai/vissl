57afc8d2a1178fa2b6bf47a2f444145c7cc933bb
Peter Kőrösi-Szabó <koszpe@gmail.com>
2022-02-18 17:27:13 +0100

    fix build

--- renyi_container/build.sh
+++ renyi_container/build.sh
@@ -3,4 +3,4 @@ source .dockerenv
 
 cd ../docker/ && image=cu102-conda source build_docker.sh
 
-docker build --rm -t ${IMAGE_NAME} --build-arg "BASE_IMAGE=${IMAGE_TAG}" .
+cd ../renyi_container && docker build --rm -t ${IMAGE_NAME} --build-arg "BASE_IMAGE=${IMAGE_TAG}" .
