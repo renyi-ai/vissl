0849bb7c72dc656f4ef646307c913eb1e1b4bf4b
Peter Kőrösi-Szabó <koszpe@gmail.com>
2022-02-18 17:21:52 +0100

    build our own image

--- renyi_container/build.sh
+++ renyi_container/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source .dockerenv
 
-cd ../docker/ && image=cu102-conda source /build_docker.sh
+cd ../docker/ && image=cu102-conda source build_docker.sh
 
-docker tag ${IMAGE_TAG} ${IMAGE_NAME}
+docker build --rm -t ${IMAGE_NAME} --build-arg "BASE_IMAGE=${IMAGE_TAG}" .
