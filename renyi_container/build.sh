3bd0d24c8d1bc8cdedf9e9c55f1af777e866bc30
Peter Kőrösi-Szabó <koszpe@gmail.com>
2022-02-18 16:49:03 +0100

    remove pip installed vissl from container

--- renyi_container/build.sh
+++ renyi_container/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source .dockerenv
 
-cd ../docker/ && image=cu102 ./build_docker.sh
+cd ../docker/ && image=cu102-conda source /build_docker.sh
 
 docker tag ${IMAGE_TAG} ${IMAGE_NAME}
